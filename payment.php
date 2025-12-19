599d4d53b39f76fec7d37b900b3568b9be9359fd
AngelaC-28 <ramcc2023-2845-63832@bicol-u.edu.ph>
2025-12-19 13:45:15 +0800

    Delete payment.php

--- payment.php
+++
@@ -1,6 +0,0 @@
-<h2>Select Payment Method</h2>
-<form method="post" action="success.php">
-  <input type="radio" name="payment" value="gcash" required> GCash<br>
-  <input type="radio" name="payment" value="maya" required> Maya<br>
-  <button type="submit">Pay Now</button>
-</form>
